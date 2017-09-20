005600c565591bc72ff1b062a9d05e72f63ff0b7
nishadhperera <nishadhperera@gmail.com>
2017-09-20 16:24:29 +0300

    scatter plot for problamatic device vendors

--- scatter_plot.py
+++ scatter_plot.py
@@ -2,17 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scapy.all import *
 
-pcap_folder = "F:\\MSC\\Master Thesis\\Network traces\\captures_IoT_Sentinel\\Test1\\Aria\\Setup-A-2-STA.pcap"
-
-dataset_X = pickle.load(open("behav_features.pickle", "rb"))
-dataset_y = pickle.load(open("behav_Devices.pickle", "rb"))
+dataset_X = pickle.load(open("Ven_behav_features.pickle", "rb"))
+dataset_v = pickle.load(open("Ven_behav_vendors.pickle", "rb"))
+dataset_y = pickle.load(open("Ven_behav_Devices.pickle", "rb"))
 print("Pickling successful behavioral features ......")
 
 dataset_X = np.array(dataset_X)
+dataset_v = np.array(dataset_v)
 dataset_y = np.array(dataset_y)
 
 # scatterplot code
-d_set = ["D-LinkSensor", "D-LinkSiren", "D-LinkWaterSensor", "EdimaxPlug1101W", "EdimaxPlug2101W", "SmarterCoffee", "iKettle2", "TP-LinkPlugHS100", "HueBridge"]
+# d_set = ["D-LinkSensor", "D-LinkSiren", "D-LinkWaterSensor", "EdimaxPlug1101W", "EdimaxPlug2101W", "SmarterCoffee", "iKettle2", "TP-LinkPlugHS100", "HueBridge"]
+# d_set = ["Aria", "HueBridge", "Lightify", "MAXGateway"]
+# d_set = ["Aria_V", "D-Link_V", "Edimax_V", "Ednet_V", "HomeMaticPlug_V", "Hue_V", "Lightify_V", "MAXGateway_V",
+#          "SmarterCoffee_V", "TP-Link_V", "WeMo_V", "Withings_V", "iKettle2_V"]
+d_set = ["SmarterCoffee_V", "iKettle2_V", "D-Link_V", "WeMo_V"]
 
 arr_ethlen_max = [[]]
 arr_iplen_max = [[]]
@@ -22,12 +26,11 @@ for i in range(len(d_set)-1):
     arr_iplen_max.append([])
 
 for j, (d) in enumerate(d_set):
-    var_X = dataset_X[dataset_y == d]
+    var_X = dataset_X[dataset_v == d]
     print(d)
     for i in range(len(var_X)):
-        print(var_X[i][74], var_X[i][79])
-        arr_ethlen_max[j].append(var_X[i][74])
-        arr_iplen_max[j].append(var_X[i][79])
+        arr_ethlen_max[j].append(var_X[i][87])
+        arr_iplen_max[j].append(var_X[i][74])
     print("------------------------------------")
 
 print(arr_ethlen_max)
@@ -38,7 +41,9 @@ N = len(arr_ethlen_max[0])
 area = np.pi * (15 * np.random.rand(N))**2  # 0 to 15 point radii
 
 for i in range(len(arr_ethlen_max)):
-    plt.scatter(arr_ethlen_max[i], arr_iplen_max[i], s=area, label=d_set[i], alpha=0.5)
+    plt.scatter(arr_ethlen_max[i], arr_iplen_max[i], label=d_set[i], alpha=0.5)
+    plt.xlabel("IP packet size variance")
+    plt.ylabel("Maximum Ethernet packet size")
 
 plt.legend()
 plt.show()
