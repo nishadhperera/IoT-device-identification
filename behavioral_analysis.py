c896c60dfd74d51a4fa8753bbb0f32f1b11a6647
nishadhperera <nishadhperera@gmail.com>
2017-09-20 16:27:37 +0300

    bug fixed in periodic calculations (timeimg issue)

--- behavioral_analysis.py
+++ behavioral_analysis.py
@@ -134,7 +134,6 @@ def calc_IA_features(packet_list, filter_con):
             pass
         else:
             IA_times.append(packet.time - prev_packet.time)
-        # prev_packet = packet
         yield packet, dev_name
 
     IA_times_list.append(IA_times)
@@ -167,16 +166,14 @@ def calc_IA_features(packet_list, filter_con):
     print(len(IA_times_list[0]))
     print(IA_times_list)
 
-    plot_list(IA_times_list[0], "Inter arrival time variation with the packet count (%s traffic)" % filter_con, "Packet count",
-              "Inter arrival time (s)")
+    # plot_list(IA_times_list[0], "Inter arrival time variation with the packet count (%s traffic)" % filter_con, "Packet Number",
+    #           "Inter arrival time (s)")
 
 
 def calc_periodic_statistics(packet_list, period_in_minutes, filter_con):
     global prev_packet
     start_time = 0
-    time_period = period_in_minutes * 60
     IA_array = []
-
     min_array = []
     max_array = []
     q1_IAT_array = []
@@ -186,14 +183,15 @@ def calc_periodic_statistics(packet_list, period_in_minutes, filter_con):
     var_IAT_array = []
     iqr_IAT_array = []
 
+    periodic_time = period_in_minutes * 60
+
     for i, (packet, dev_name) in enumerate(packet_list):
         if start_time == 0:
-            print("Start time is set")
             start_time = packet.time
         if prev_packet == "":
             pass
         else:
-            if (packet.time - start_time) <= time_period:
+            if (packet.time - start_time) <= periodic_time:
                 IA_array.append(packet.time - prev_packet.time)
             else:
                 print("CALCULATE SUMMARY STATISTICS")
@@ -205,31 +203,30 @@ def calc_periodic_statistics(packet_list, period_in_minutes, filter_con):
                 q3_IAT_array.append(np.percentile(IA_array, 75))  # third quartile of inter-arrival time
                 var_IAT_array.append(np.var(IA_array))  # variance of inter-arrival time
                 iqr_IAT_array.append(np.percentile(IA_array, 75) - np.percentile(IA_array, 25))  # inter quartile range of inter-arrival time
-                start_time = packet.time
+                start_time += periodic_time
                 IA_array = []
         prev_packet = packet
         yield packet, dev_name
 
-    plot_list(min_array, "Minimum IAT per hr with the packet count (%s)" % filter_con,
-              "Packet count", "Inter arrival time (s)")
-    plot_list(max_array, "Maximum IAT per hr with the packet count (%s)" % filter_con,
-              "Packet count", "Inter arrival time (s)")
-    plot_list(q1_IAT_array, "IAT Q1 per hr with the packet count (%s)" % filter_con,
-              "Packet count", "Inter arrival time (s)")
-    plot_list(median_IAT_array, "Median IAT per hr with the packet count (%s)" % filter_con,
-              "Packet count", "Inter arrival time (s)")
-    plot_list(mean_IAT_array, "Mean IAT per hr with the packet count (%s)" % filter_con,
-              "Packet count", "Inter arrival time (s)")
-    plot_list(q3_IAT_array, "IAT Q3 per hr with the packet count (%s)" % filter_con,
-              "Packet count", "Inter arrival time (s)")
-    plot_list(var_IAT_array, "Variance of IAT per hr with the packet count (%s)" % filter_con,
-              "Packet count", "Inter arrival time (s)")
-    plot_list(iqr_IAT_array, "IAT IQR per hr with the packet count (%s)" % filter_con,
-              "Packet count", "Inter arrival time (s)")
+    # plot_list(min_array, "Minimum IAT per hr with the packet count (%s)" % filter_con,
+    #           "Packet count", "Inter arrival time (s)")
+    # plot_list(max_array, "Maximum IAT per hr with the packet count (%s)" % filter_con,
+    #           "Packet count", "Inter arrival time (s)")
+    # plot_list(q1_IAT_array, "IAT Q1 per hr with the packet count (%s)" % filter_con,
+    #           "Packet count", "Inter arrival time (s)")
+    # plot_list(median_IAT_array, "Median IAT per hr with the packet count (%s)" % filter_con,
+    #           "Packet count", "Inter arrival time (s)")
+    # plot_list(mean_IAT_array, "Mean IAT per hr with the packet count (%s)" % filter_con,
+    #           "Packet count", "Inter arrival time (s)")
+    # plot_list(q3_IAT_array, "IAT Q3 per hr with the packet count (%s)" % filter_con,
+    #           "Packet count", "Inter arrival time (s)")
+    # plot_list(var_IAT_array, "Variance of IAT per hr with the packet count (%s)" % filter_con,
+    #           "Packet count", "Inter arrival time (s)")
+    # plot_list(iqr_IAT_array, "IAT IQR per hr with the packet count (%s)" % filter_con,
+    #           "Packet count", "Inter arrival time (s)")
 
 
 def calc_protocol_freq(packet_list, period_in_minutes, filter_con):
-    global prev_packet
     tcp_list = []
     udp_list = []
     http_list = []
@@ -242,39 +239,88 @@ def calc_protocol_freq(packet_list, period_in_minutes, filter_con):
     dns_counter = 0
     start_time = 0
 
-    time_period = period_in_minutes * 60
+    periodic_time = period_in_minutes * 60
 
     for i, (packet, dev_name) in enumerate(packet_list):
         if start_time == 0:
-            print("Start time is set")
             start_time = packet.time
-        if (packet.time - start_time) <= time_period:
-            tcp_val, udp_val, tl_pro = fe.get_tcpudp_feature(packet)
+
+        tcp_val, udp_val, tl_pro = fe.get_tcpudp_feature(packet)
+        http_val = fe.get_http_feature(packet, tl_pro)
+        bootp_val, dhcp_val = fe.get_bootp_dhcp_feature(packet, tl_pro)
+        dns_val = fe.get_dns_feature(packet, tl_pro)
+
+        if (packet.time - start_time) <= periodic_time:
             tcp_counter = tcp_counter + tcp_val
             udp_counter = udp_counter + udp_val
-            http_counter = http_counter + fe.get_http_feature(packet, tl_pro)
-            bootp_val, dhcp_val = fe.get_bootp_dhcp_feature(packet, tl_pro)
+            http_counter = http_counter + http_val
             dhcp_counter = dhcp_counter + dhcp_val
-            dns_counter = dns_counter + fe.get_dns_feature(packet, tl_pro)
+            dns_counter = dns_counter + dns_val
         else:
             tcp_list.append(tcp_counter)
             udp_list.append(udp_counter)
             http_list.append(http_counter)
             dhcp_list.append(dhcp_counter)
             dns_list.append(dns_counter)
-            tcp_counter = 0
-            udp_counter = 0
-            http_counter = 0
-            dhcp_counter = 0
-            dns_counter = 0
-            start_time = packet.time
+            tcp_counter = tcp_val
+            udp_counter = udp_val
+            http_counter = http_val
+            dhcp_counter = dhcp_val
+            dns_counter = dns_val
+            start_time += periodic_time
         yield packet, dev_name
 
-    plot_list(tcp_list, "Number of TCP packets per hr (%s)" % filter_con, "hr", "Packet count")
-    plot_list(udp_list, "Number of UDP packets per hr (%s)" % filter_con, "hr", "Packet count")
-    plot_list(http_list, "Number of HTTP packets per hr (%s)" % filter_con, "hr", "Packet count")
-    plot_list(dhcp_list, "Number of DHCP packets per hr (%s)" % filter_con, "hr", "Packet count")
-    plot_list(dns_list, "Number of DNS packets per hr (%s)" % filter_con, "hr", "Packet count")
+    # plot_list(tcp_list, "Number of TCP packets per hr (%s)" % filter_con, "hr", "Packet count")
+    # plot_list(udp_list, "Number of UDP packets per hr (%s)" % filter_con, "hr", "Packet count")
+    # plot_list(http_list, "Number of HTTP packets per hr (%s)" % filter_con, "hr", "Packet count")
+    # plot_list(dhcp_list, "Number of DHCP packets per hr (%s)" % filter_con, "hr", "Packet count")
+    # plot_list(dns_list, "Number of DNS packets per hr (%s)" % filter_con, "hr", "Packet count")
+
+
+def calc_pkt_rate(packet_list, period_in_minutes, filter_con):
+    start_time = 0
+    packet_count = 0
+    pkt_rate = []
+
+    periodic_time = period_in_minutes * 60
+
+    for i, (packet, dev_name) in enumerate(packet_list):
+        if start_time == 0:
+            start_time = packet.time
+        if (packet.time - start_time) <= periodic_time:
+            packet_count += 1
+        else:
+            pkt_rate.append(packet_count)
+            packet_count = 1
+            start_time += periodic_time
+        yield packet, dev_name
+
+    # plot_list(pkt_rate, "Packet Rate (%s)" % filter_con, "minutes", "Packet count")
+
+
+def calc_pkt_order(packet_list, filter_con):
+    pkt_order = []
+
+    for i, (packet, dev_name) in enumerate(packet_list):
+        tcp_val, udp_val, tl_pro = fe.get_tcpudp_feature(packet)
+        if tl_pro == "TCP":
+            pkt_order.append(1)
+        elif tl_pro == "UDP":
+            pkt_order.append(2)
+        else:
+            pkt_order.append(0)
+        yield packet, dev_name
+
+    # plot_list(pkt_order, "Packet Order (%s)" % filter_con, "minutes", "Packet type (TCP=1, UDP=2, Other=0)")
+
+
+def calc_payload_len(packet_list, filter_con):
+    payload_lengths = []
+
+    for i, (packet, dev_name) in enumerate(packet_list):
+        payload_lengths.append(fe.get_payload_feature(packet))
+
+    plot_list(payload_lengths, "Packet payload lengths (%s)" % filter_con, "Packet Number", "Payload length")
 
 
 def end_generator(packet_list):
@@ -296,12 +342,14 @@ def load_behavior_features(folder):
     sample_time = 1    # time in minutes
     piped_to_periodic_stat = calc_IA_features(piped_to_IA, filter)
     piped_to_prot_freq = calc_periodic_statistics(piped_to_periodic_stat, sample_time, filter)
-    piped_to_end_generator = calc_protocol_freq(piped_to_prot_freq, sample_time, filter)
+    piped_to_pkt_rate = calc_protocol_freq(piped_to_prot_freq, sample_time, filter)
+    piped_to_pkt_order = calc_pkt_rate(piped_to_pkt_rate, sample_time, filter)
+    piped_to_payload_len = calc_pkt_order(piped_to_pkt_order, filter)
+    piped_to_end_generator = calc_payload_len(piped_to_payload_len, filter)
     end_generator(piped_to_end_generator)
 
     return feature_list, device_list
 
 pcap_folder = "F:\\MSC\\Master Thesis\\Network traces\\Behavioral_captures\\"
-
 
 dataset_X, dataset_y = load_behavior_features(pcap_folder)
