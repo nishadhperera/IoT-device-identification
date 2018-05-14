799b136ba5bf7c3428f3d00cfa7b207f89157b41
Nishadh Aluthge <nishadh.aluthge@helsinki.fi>
2018-05-14 16:34:01 +0100

    Added operational paramaters

--- Hue.py
+++ Hue.py
@@ -1,23 +1,33 @@
+# This application supports operation of a Philips Hue smart bulb using Python
+
 from phue import Bridge
 from time import sleep
 
-b = Bridge('192.168.1.168')
+IP = "192.168.1.168"
+b = Bridge(IP)
 
-# If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
-b.connect()
 
-# Get the bridge state (This returns the full dictionary that you can explore)
-b.get_api()
+def connect(b):
+    # If the app is not registered and the button is not pressed, press the button and call connect()
+    # (this only needs to be run a single time)
+    b.connect()
+    b.get_api()     # Get the bridge state (This returns the full dictionary that you can explore)
 
-lights = b.lights
 
-# Print light names
-for l in lights:
-    print(l.name)
+def list_bulbs(b):
+    lights = b.lights
+    for l in lights:        # Print light names
+        print(l.name)
 
-b.set_light(2,'on', True)
-b.set_light(2, 'bri', 2)
+
+def set_params(b, light_ID, param, value):
+    # Example b.set_light(2,'on', True), b.set_light(2, 'bri', 200)
+    b.set_light(light_ID, param, value)
+
+
+# Operating the Hue bulb
+connect(b)      # connecting to the Huw bulb
+list_bulbs(b)   # lisitng available bulbs
+set_params(2,'on', True)
 sleep(4)
-b.set_light(2, 'bri', 200)
-sleep(4)
-b.set_light(2,'on', False)
+set_params(2, 'bri', 200)
